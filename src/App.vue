5a7ec88944df468754ea6d8d8cf2c5e01b8c7d9f
Ben Kerllenevich <me@notom3ga.me>
2021-07-01 20:15:22 -0400

    experimental light mode

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,14 @@
 
 <script>
 export default {
-  name: 'Arc'
+  name: 'Arc',
+  mounted() {
+    switch (localStorage.getItem('theme').toLowerCase()) {
+      case 'light':
+        for (const element of document.getElementsByTagName('*')) {
+          element.setAttribute('theme', 'light')
+        }
+    }
+  }
 }
 </script>
