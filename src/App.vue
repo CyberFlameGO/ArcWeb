944fa31d91252391a6fb2be3ff3e29ac57e3889d
Ben Kerllenevich <me@notom3ga.me>
2021-07-02 11:58:59 -0400

    more graph work

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,10 @@
 export default {
   name: 'App',
   mounted() {
+    if (localStorage.getItem('theme') == null) {
+      localStorage.setItem('theme', 'dark')
+    }
+
     switch (localStorage.getItem('theme').toLowerCase()) {
       case 'light':
         for (const element of document.getElementsByTagName('*')) {
--- src/components/viewer/Graph.vue
+++ src/components/viewer/Graph.vue
@@ -1,5 +1,4 @@
 <template>
-  todo: graph
 </template>
 
 <script>
